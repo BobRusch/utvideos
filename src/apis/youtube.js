6b0d480aa69f1c87eeffca2af0da1804df355b25
Bob Rusch <rvrusch@gmail.com>
2020-11-04 07:12:56 -0500

    msg

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// const KEY = 'AIzaSyB3_XD8TY1jClHFhC0q5F2Uo_72rFYmtuU';
-
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
